a50ddf5364af3bed437ea836482193ac9e642783
Alex <alex.crownshaw12@gmail.com>
2023-10-30 13:10:33 +0000

    Config XOr inputs and exp outputs

--- main.cpp
+++ main.cpp
@@ -5,31 +5,26 @@
 
 int main() {
 
-    // // Define parameters to pass to Neuron class
-	// std::vector <Node> links{};
-	// std::vector <double> link_weights{0.f, 1.f, 2.f};
+	std::vector <int> structure {2, 2, 1};
+	std::vector <std::vector<double>> inputs {};
+	std::vector <std::vector<double>> outputs {};
 
-    // // Create Neuron class
-	// Node n1(links, link_weights);
+	std::vector <double> input_vars {0.f, 1.f};
 
-    // // Print local variables and object variables
-	// std::cout << "main_weights: " << link_weights.at(0) << std::endl;
-	// std::cout << "class_weights: " << n1.getWeights().at(2) << std::endl;
+	// Create input and expected output vectors for XOr operation
+	for (int i_var {}; i_var < input_vars.size(); i_var++)	{
+		for (int j_var {}; j_var < input_vars.size(); j_var++)	{
+			// Populate inputs
+			inputs.push_back(std::vector <double> {input_vars.at(i_var), input_vars.at(j_var)});
 
-    // // Create subset of vector and print all elements
-    // std::vector<double> class_weights {n1.getWeights()};
-    // std::vector<double> sub_weights {std::vector<double>(n1.getWeights().begin() + 1, n1.getWeights().end())};
-	// for (size_t i{}; i < sub_weights.size(); i++)	{
-	// 	std::cout << "sub_weights: " << sub_weights.at(i) << std::endl;
-	// }
+			// Compute XOr output
+			outputs.push_back(std::vector <double> {abs(input_vars.at(i_var) - input_vars.at(j_var))});
+		}
+	}
 
-	// Link link;
-	// link.weight = 1.0;
+	Network nn(structure);
+	nn.setInputs(inputs);
+	nn.setExpOutputs(outputs);
 
-	std::vector <int> structure_main {2, 2, 1};
-	Network nn(structure_main);
-	
-	std::vector <Node> layer_0_node_0_links {nn.nodes.at(0).at(0).getLinks()};
-	std::vector <double> layer_0_node_0_weights {nn.nodes.at(0).at(0).getWeights()};
-	std::cout << layer_0_node_0_weights.size();
+	std::cout << "stop";
 }
