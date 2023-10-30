ba96dbac708cd3f70006f1151513e18c98d0d45f
Alex <alex.crownshaw12@gmail.com>
2023-10-30 09:38:53 +0000

    Creates network of forward linked nodes

--- main.cpp
+++ main.cpp
@@ -4,21 +4,29 @@
 #include "simple_neuromorphic_toolbox.cpp"
 
 int main() {
-    // Define parameters to pass to Neuron class
-	std::vector <Neuron> links{};
-	std::vector <int> link_weights{0, 1, 2};
-
-    // Create Neuron class
-	Neuron n1(links, link_weights);
-
-    // Print local variables and object variables
-	std::cout << "main_weights: " << link_weights.at(0) << std::endl;
-	std::cout << "class_weights: " << n1.weights.at(2) << std::endl;
-
-    // Create subset of vector and print all elements
-    std::vector<int> sub_weights {std::vector<int>(n1.weights.begin() + 1, n1.weights.end())};
-	for (size_t i{}; i < sub_weights.size(); i++)	{
-		std::cout << "sub_weights: " << sub_weights.at(i) << std::endl;
-	}
 	
+    // // Define parameters to pass to Neuron class
+	// std::vector <Node> links{};
+	// std::vector <double> link_weights{0.f, 1.f, 2.f};
+
+    // // Create Neuron class
+	// Node n1(links, link_weights);
+
+    // // Print local variables and object variables
+	// std::cout << "main_weights: " << link_weights.at(0) << std::endl;
+	// std::cout << "class_weights: " << n1.getWeights().at(2) << std::endl;
+
+    // // Create subset of vector and print all elements
+    // std::vector<double> class_weights {n1.getWeights()};
+    // std::vector<double> sub_weights {std::vector<double>(n1.getWeights().begin() + 1, n1.getWeights().end())};
+	// for (size_t i{}; i < sub_weights.size(); i++)	{
+	// 	std::cout << "sub_weights: " << sub_weights.at(i) << std::endl;
+	// }
+
+	// Link link;
+	// link.weight = 1.0;
+
+	std::vector <int> structure_main {2, 2, 2};
+	std::cout << structure_main.size();
+	Network nn(structure_main);
 }
--- simple_neuromorphic_toolbox.cpp
+++ simple_neuromorphic_toolbox.cpp
@@ -1,23 +1,94 @@
 #include <vector>
 #include <iostream>
 
+// class Node;
 
-class Neuron {
+// struct Link {
+// public:
+
+//     Node node;
+//     double weight;
+
+// };
+
+class Node {
+private:
+
+	// std::vector <Link> links;
+	std::vector <Node> links;
+	std::vector <double> weights;
+
 public:
 
-	std::vector <Neuron> links;
-	std::vector <int> weights;
+	// // links.getter
+	// std::vector <Link> getLinks()	{
+	// 	return links;
+	// }
 
-	Neuron(std::vector <Neuron> links_, std::vector <int> weights_) {
-		links = links_;
-		weights = weights_;
+	// links.getter
+	std::vector <Node> getLinks()	{
+		return links;
+	}
+
+	// // links.setter
+	// void setLinks(std::vector <Node> node_values, std::vector <double> weight_values)	{
+	// 	links.reserve(node_values.size());
+	// 	for (size_t i_node; i_node < node_values.size(); i_node++)	{
+	// 		links.at(i_node) = Link();
+	// 		links.at(i_node).node = node_values.at(i_node);
+	// 		links.at(i_node).weight = weight_values.at(i_node);
+	// 	}
+	// }
+
+	// links.setter
+	void setLinks(std::vector <Node> value)	{
+		links = value;
+	}
+
+	// weights.getter
+	std::vector <double> getWeights()	{
+		return weights;
+	}
+
+	// weights.setter
+	void setWeights(std::vector <double> value)	{
+		weights = value;
 	}
 };
 
 class Network {
 public:
+	std::vector <int> structure;
+	std::vector <std::vector<Node>> nodes {};
 
-	Network() {
+// public:
 
+	Network(std::vector <int> structure) {
+		// structure = structure_value;
+		std::cout << structure.size();
+
+		// Pre-allocate size of layer/node vectors
+		// nodes.reserve(structure.size());
+		for (int i_layer {}; i_layer < structure.size(); i_layer++)	{
+			// nodes.at(i_layer).reserve(structure.at(i_layer));
+
+			// Create Node objects for layer
+			std::vector <Node> layer_nodes {};
+			for (int i_node {0}; i_node < structure.at(i_layer); i_node++)	{
+				// nodes.at(i_layer).at(i_node) = Node();
+				layer_nodes.push_back(Node());
+			}
+			nodes.push_back(layer_nodes);
+
+			// Attach Node objects to previous layer as Link structs
+			if (i_layer != 0)	{
+				for (int i_node {0}; i_node < nodes.at(i_layer - 1).size(); i_node++)	{
+					nodes.at(i_layer - 1).at(i_node).setLinks(nodes.at(i_layer));
+					std::vector<double> weights(1.f, nodes.at(i_layer).size());
+					nodes.at(i_layer).at(i_node).setWeights(weights);
+					// nodes.at(i_layer - 1).at(i_node).setLinks(nodes.at(i_layer), weights);
+				}
+			}
+		}
 	}
 };
