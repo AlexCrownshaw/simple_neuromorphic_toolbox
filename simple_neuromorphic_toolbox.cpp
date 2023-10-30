d7906ddadb82d7ea8848f3edbd8dd92ad8d145b5
Alex <alex.crownshaw12@gmail.com>
2023-10-30 13:09:59 +0000

    Adds input and exp output properties

--- simple_neuromorphic_toolbox.cpp
+++ simple_neuromorphic_toolbox.cpp
@@ -1,44 +1,24 @@
 #include <vector>
 #include <iostream>
 
-// class Node;
 
-// struct Link {
-// public:
+struct Epoch	{
+	double learning_eff {};
 
-//     Node node;
-//     double weight;
-
-// };
+};
 
 class Node {
 private:
 
-	// std::vector <Link> links;
 	std::vector <Node> links;
 	std::vector <double> weights;
 
 public:
 
-	// // links.getter
-	// std::vector <Link> getLinks()	{
-	// 	return links;
-	// }
-
 	// links.getter
 	std::vector <Node> getLinks()	{
 		return links;
 	}
-
-	// // links.setter
-	// void setLinks(std::vector <Node> node_values, std::vector <double> weight_values)	{
-	// 	links.reserve(node_values.size());
-	// 	for (size_t i_node; i_node < node_values.size(); i_node++)	{
-	// 		links.at(i_node) = Link();
-	// 		links.at(i_node).node = node_values.at(i_node);
-	// 		links.at(i_node).weight = weight_values.at(i_node);
-	// 	}
-	// }
 
 	// links.setter
 	void setLinks(std::vector <Node> value)	{
@@ -57,37 +37,73 @@ public:
 };
 
 class Network {
-public:
+private:
+
 	std::vector <int> structure;
 	std::vector <std::vector<Node>> nodes {};
+	std::vector <std::vector<double>> inputs {};
+	std::vector <std::vector<double>> exp_outputs {};
 
-// public:
+public:
 
 	Network(std::vector <int> structure_value) {
 		structure = structure_value;
+		createNetwork();
+	}
 
-		// Pre-allocate size of layer/node vectors
-		// nodes.reserve(structure.size());
+	void createNetwork()	{
 		for (int i_layer {}; i_layer < structure.size(); i_layer++)	{
-			// nodes.at(i_layer).reserve(structure.at(i_layer));
 
 			// Create Node objects for layer
 			std::vector <Node> layer_nodes {};
 			for (int i_node {0}; i_node < structure.at(i_layer); i_node++)	{
-				// nodes.at(i_layer).at(i_node) = Node();
 				layer_nodes.push_back(Node());
 			}
 			nodes.push_back(layer_nodes);
 
-			// Attach Node objects to previous layer as Link structs
+			// Attach Node objects and weights to previous layer
 			if (i_layer != 0)	{
 				for (int i_node {0}; i_node < nodes.at(i_layer - 1).size(); i_node++)	{
 					nodes.at(i_layer - 1).at(i_node).setLinks(nodes.at(i_layer));
 					std::vector<double> weights(nodes.at(i_layer).size(), 1.f);
 					nodes.at(i_layer - 1).at(i_node).setWeights(weights);
-					// nodes.at(i_layer - 1).at(i_node).setLinks(nodes.at(i_layer), weights);
 				}
 			}
 		}
+	}
+
+	Epoch runEpoch()	{
+		Epoch epoch;
+		return epoch;
+	}
+
+	// structure.getter
+	std::vector <int> getStructure()	{
+		return structure;
+	}
+
+	// nodes.getter
+	std::vector <std::vector<Node>> getNodes()	{
+		return nodes;
+	}
+
+	// inputs.setter
+	void setInputs(std::vector <std::vector<double>> value)	{
+		inputs = value;
+	}
+
+	// inputs.getter
+	std::vector <std::vector<double>> getInputs()	{
+		return inputs;
+	}
+
+	// exp_outputs.setter
+	void setExpOutputs(std::vector <std::vector<double>> value)	{
+		exp_outputs = value;
+	}
+
+	// exp_outputs.getter
+	std::vector <std::vector<double>> getExpOutputs()	{
+		return exp_outputs;
 	}
 };
